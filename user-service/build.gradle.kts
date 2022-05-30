c27aa99015f3e28a0e3452b92dc5b611298f6fc7
soroush-bn <soroush.baghernezhad3@gmail.com>
2022-05-30 23:37:17 +0430

    * adding gRPC dependencies

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.0"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
-	kotlin("plugin.jpa") version "1.6.21"
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+//    id("com.google.protobuf") version "0.8.18"
 }
 
 group = "com.cloud"
@@ -41,6 +42,19 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+
+    //gRPC
+//    protobuf(project(":protos"))
+//
+//    api(kotlin("stdlib"))
+//    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${rootProject.ext["coroutinesVersion"]}")
+//
+//    api("io.grpc:grpc-stub:${rootProject.ext["grpcVersion"]}")
+//    api("io.grpc:grpc-protobuf:${rootProject.ext["grpcVersion"]}")
+//    api("com.google.protobuf:protobuf-java-util:${rootProject.ext["protobufVersion"]}")
+//    api("com.google.protobuf:protobuf-kotlin:${rootProject.ext["protobufVersion"]}")
+//    api("io.grpc:grpc-kotlin-stub:${rootProject.ext["grpcKotlinVersion"]}")
+
 }
 extra["springCloudVersion"] = "2021.0.3-SNAPSHOT"
 dependencyManagement {
@@ -58,3 +72,40 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${rootProject.ext["protobufVersion"]}"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${rootProject.ext["grpcVersion"]}"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:${rootProject.ext["grpcKotlinVersion"]}:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
+/*
+// this makes it so IntelliJ picks up the sources but then ktlint complains
+sourceSets {
+    val main by getting { }
+    main.java.srcDirs("build/generated/source/proto/main/java")
+    main.java.srcDirs("build/generated/source/proto/main/grpc")
+    main.java.srcDirs("build/generated/source/proto/main/kotlin")
+    main.java.srcDirs("build/generated/source/proto/main/grpckt")
+}
+ */
